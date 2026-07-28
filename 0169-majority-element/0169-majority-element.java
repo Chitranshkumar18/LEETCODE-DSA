63145eafcbffc3b5695d29deba20ae686bd3436e
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-07-29 01:38:12 +0530

    Update 0169-majority-element.java

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -13,3 +13,13 @@ class Solution {
         return candidate;
     }
 }
+
+
+
+
+    public int majorityElement(int[] arr) {
+        int n= arr.length;
+        Arrays.sort(arr);
+        return arr[n/2];
+        
+    }
